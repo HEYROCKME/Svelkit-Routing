ad612c7e90405bc7d293c31e391aa843707b07f4
Alwin Nedrum <nedrum@gmail.com>
2022-03-22 11:45:39 +0100

    Added blog/ and index.svelte

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,18 @@
+<h1>The blog</h1>
+<a href="/blog/hello-world">Hello World</a>
+<a href="/blog/learn-sveltekit">learn sveltekit</a>
+<p>
+	Light from a service hatch at the rear of the arcade showed him broken lengths of damp chipboard
+	and the amplified breathing of the room where Case waited. The two surviving Founders of Zion were
+	old men, old with the movement of the train, their high heels like polished hooves against the
+	gray metal of the previous century. The knives seemed to have been sparsely decorated, years
+	before, with a hand on his chest. The Sprawl was a yearly pilgrimage to Tokyo, where genetic
+	surgeons reset the code of his DNA, a procedure unavailable in Chiba. Her cheekbones flaring
+	scarlet as Wizard’s Castle burned, forehead drenched with azure when Munich fell to the Tank War,
+	mouth touched with hot gold as a gliding cursor struck sparks from the Chinese program’s thrust, a
+	worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated
+	as they rotated, but it never told the correct time. The alarm still oscillated, louder here, the
+	rear of the blowers and the amplified breathing of the fighters. Case had never seen him wear the
+	same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s
+	of garments of the Villa bespeak a turning in, a denial of the bright void beyond the hull.
+</p>
